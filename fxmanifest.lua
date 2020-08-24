29330eb5af717ea2559706a1130fe8af5ae17a4f
Slamdunk91 <49899217+Slamdunk91@users.noreply.github.com>
2020-08-24 14:48:53 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Slamdunk & Bubu'
+description 'FiveM Loading Screen'
+version '1.0.0'
+
+loadscreen 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/css/style.css',
+    'ui/js/jquery.min.js',
+    'ui/js/script.js',
+    'ui/fonts/Prime-Regular.woff2',
+    'ui/img/poster.png',
+    'ui/img/volume.svg',
+    'ui/img/volume-mute.svg',
+    'ui/img/cursor.svg'
+}
