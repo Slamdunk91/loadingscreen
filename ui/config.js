4aadec639ff8556497278fa47c9d8e9dfeaad8a9
Slamdunk91 <49899217+Slamdunk91@users.noreply.github.com>
2020-08-24 23:08:20 +0200

    Update config.js

--- ui/config.js
+++ ui/config.js
@@ -1,8 +1,8 @@
 var CONFIG = {
   debug: false,
-  video: 'https://cdn.lagoon-island.fr/videos/loading.mp4',
+  video: 'https://cdn.lagoon-island.fr/videos/loading.mp4', //source of your video.
   defaults: {
-    volume: 0.5,
-    muted: false
+    volume: 0.5, //manage the volume of the video. Value from 0 to 1.
+    muted: false //you can choose to have the sound muted for the joiners of your server, turn it to true if you want to mute.
   }
 };
